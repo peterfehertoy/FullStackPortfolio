20defd8bcbd92a0cc0825879226208aa028db9b7
peterfehertoy <petyata15@gmail.com>
2020-03-03 14:47:48 +0200

    valami

---
+++ javascript/canvasexample/public/script.js
@@ -0,0 +1,47 @@
+var canvas;
+var context;
+var running = 0;
+var interval;
+
+window.onload = function() {
+    canvas = document.getElementById("mycanvas");
+    context  = canvas.getContext("2d");
+}
+
+function createRect () {
+    let x=0;
+    let y=0;
+    let side=0;
+    let color="#";
+    let colorpicker = "0123456789ABCDEF"
+    x = Math.floor(Math.random()*400)+1;
+    y = Math.floor(Math.random()*400)+1;
+    side = Math.floor(Math.random()*80)+20;
+    for(let i=0;i<6;i++) {
+        let letters ="0123456789ABCDEF"
+        let temp = Math.floor(Math.random()*16);
+        color = color + colorpicker[temp];
+    }
+    context.fillStyle = color;
+    context.fillRect(x,y,side,side);
+}
+
+function startCanvas() {
+    if(running) {
+        running = 0;
+        clearInterval(interval);
+
+    }else {
+        running = 1;
+        interval = setInterval(createRect,200);
+    }
+}
+
+function reloadCanvas() {
+    x=0;
+    y=0;
+    side=0;
+
+    context.fillRect(x,y,side,side);
+}
+
